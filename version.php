6e77fe3935ccb70e4178a2e566c84b2c49d83cba
fairsayan <fairsayan@gmail.com>
2012-07-05 11:17:20 +0300

    Update master

--- version.php
+++ version.php
@@ -11,6 +11,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $module->version   = 2012060700;           // If version == 0 then module will not be installed
-$module->requires  = 2010031900;           // Requires this Moodle version
+$module->requires  = 2011051000;           // Requires this Moodle version
 $module->cron      = 0;                    // Period for cron to check this module (secs)
+$module->release   = '2.2.x (Build: 2011051000)';
+$module->maturity  = 'MATURITY_RC';
 $module->component = 'mod_offlinesession'; // To check on upgrade, that module sits in correct place
