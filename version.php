93ee79de70c962c8cb5156d26eb048b4bddad7c3
Domenico Pontari <fairsayan@gmail.com>
2012-07-05 10:06:31 +0200

    better format

--- version.php
+++ version.php
@@ -8,21 +8,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-/**
- * Defines the version of offlinesession
- *
- * This code fragment is called by moodle_needs_upgrading() and
- * /admin/index.php
- *
- * @package    mod
- * @subpackage offlinesession
- * @copyright  2011 Your Name
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2012060700;               // If version == 0 then module will not be installed
-$module->requires  = 2010031900;      // Requires this Moodle version
-$module->cron      = 0;               // Period for cron to check this module (secs)
+$module->version   = 2012060700;           // If version == 0 then module will not be installed
+$module->requires  = 2010031900;           // Requires this Moodle version
+$module->cron      = 0;                    // Period for cron to check this module (secs)
 $module->component = 'mod_offlinesession'; // To check on upgrade, that module sits in correct place
